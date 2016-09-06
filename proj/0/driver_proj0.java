954cf44dfa7ae20cf8a73ed2741582c3580894b6
delroy.mathieson <delroy.mathieson1@marist.edu>
2016-09-06 16:56:27 -0400

    This is Project 0

---
+++ proj/0/driver_proj0.java
@@ -0,0 +1,17 @@
+/**
+ * file: driver_proj0.java
+ * author: Delroy Mathieson
+ * course: CMPT 220
+ * assignment: Project 0
+ * due date: September 6, 2016
+ * version: 1.8.0_102
+ * 
+ * This file contains the declaration of the driver_proj0 abstract data
+ * type.
+ */
+public class driver_proj0 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+ 
